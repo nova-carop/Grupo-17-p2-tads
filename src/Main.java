e5f0a8c09f674267709712762558a66eee048eb5
Santiago Piñeyro <santiagopineyrog@gmail.com>
2025-06-12 21:51:26 -0300

    arregle impresiones de contarTiempoCarga

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         Scanner scanner = new Scanner(System.in);
-        long tiempoInicial = System.currentTimeMillis();
+        long tiempoInicial;
         boolean salir = false;
 
         while (!salir) {
